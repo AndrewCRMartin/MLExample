c484d0abde0b03a8d24d8d1880a7920e78151eb3
AndrewCRMartin <andrew@bioinf.org.uk>
2022-09-30 17:07:37 +0100

    Finalized example1

--- example1/demo.sh
+++ example1/demo.sh
@@ -2,9 +2,10 @@
 # Note that CSV2arff will do the balancing for you by using -limit
 
 # Weka 
-export WEKA=/home/amartin/weka-3-8-3
+export WEKA=$HOME/weka
 export CLASSPATH="$WEKA/weka.jar"
 
+echo "Creating dataset"
 # Split up the demo csv file into train and test sets
 # (I know the size of the file do have hard coded these)
 head -50001 demo.csv > train.csv
@@ -15,6 +16,7 @@ tail -11205 demo.csv >> test.csv
 # for the machine learning
 
 # Convert training data to arff format
+echo "Converting training data to arff format"
 csv2arff -skip -ni inputs.dat dataset train.csv >train.arff
 # -skip      - skip records with missing values
 # -ni        - do not convert binary inputs to nominal Boolean
@@ -31,11 +33,14 @@ NTREE=100
 
 # This trains, saves the trained model and tests in one go
 # To train without the independent testing remove the '-T test.arff'
+echo "Training the classifier"
 java $CLASSIFIER -I $NTREE -t train.arff -d train.model -T test.arff > test.out
 
 # This tests on a pre-trained model
+echo "Testing the classifier"
 java $CLASSIFIER -l train.model -T test.arff > test2.out
 
+echo "Results are in test2.out"
 
 # Cleanup
 # rm train.csv test.csv train.arff test.arff
